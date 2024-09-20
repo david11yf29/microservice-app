8ff1a060d57a56a5585d50c227f6895bfa2e4114
David Lin <david11yf29@gmail.com>
2024-09-20 16:07:56 -0700

    interBranch set up

--- order-service/src/main/java/com/microservice/service/OrderService.java
+++ order-service/src/main/java/com/microservice/service/OrderService.java
@@ -8,6 +8,7 @@ import com.microservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.UUID;
@@ -18,8 +19,9 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
-    public void placeOrder(OrderRequest orderRequest) {
+    public void placeOrder(OrderRequest orderRequest) throws IllegalAccessException {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
@@ -30,7 +32,20 @@ public class OrderService {
                 .toList();
 
         order.setOrderLineItemsList(orderLineItemsList);
-        orderRepository.save(order);
+
+        // Call Inventory Service, and place order if the product is in the stock
+        Boolean result = webClient.get()
+                .uri("http://localhost:8082/api/inventory")
+                .retrieve()
+                .bodyToMono(Boolean.class)
+                .block();
+
+        if (result) {
+            orderRepository.save(order);
+        } else {
+            throw new IllegalAccessException("Product is not in stock, please try again later.");
+        }
+
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {

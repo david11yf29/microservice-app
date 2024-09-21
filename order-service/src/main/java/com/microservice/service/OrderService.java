8e66b36e9f03e3e0e66b678ce20d133a3f382a1a
David Lin <david11yf29@gmail.com>
2024-09-20 23:05:42 -0700

    discoveryBranch coding 2

--- order-service/src/main/java/com/microservice/service/OrderService.java
+++ order-service/src/main/java/com/microservice/service/OrderService.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) throws IllegalAccessException {
         Order order = new Order();
@@ -41,8 +41,8 @@ public class OrderService {
                 .toList();
 
         // Call Inventory Service, and place order if the product is in the stock
-        InventoryResponse[] inventoryResponsesArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponsesArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
